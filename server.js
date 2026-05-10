e1621253faada3b108383cb41d8edb9a1aade7e5
Tiki-Taco <bradenzamora32@gmail.com>
2026-05-09 22:31:03 -0400

    Fix server to serve all pages

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname)));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
 app.listen(PORT, () => {
   console.log(`First Coast Flight School running on port ${PORT}`);
 });
